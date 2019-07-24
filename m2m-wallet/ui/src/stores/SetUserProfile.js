a7a4cf10a11ef473ef0a5bc7f8d6cb3cb074b900
NamGyeong <namgyeong.cho@mxc.org>
2019-07-24 15:27:42 +0200

    added hashcode compapre logic

--- m2m-wallet/ui/src/stores/SetUserProfile.js
+++ m2m-wallet/ui/src/stores/SetUserProfile.js
@@ -1,12 +1,23 @@
 import sessionStore from "./SessionStore";
+import hash from "object-hash";
 
 export default function updateOrganizations(response) {
-    /* if(response.body.userProfile.isOrgListModified){
-        sessionStore.setOrganizationList(response.body.userProfile.organizations);
-    } */
     
-    if(true){
-        sessionStore.setOrganizationList(response.body.userProfile.organizations);
-    } 
+    const organizationList = response.body.userProfile.organizations;
+    
+    if(!organizationList){
+        return false;
+    }
+    
+    if(sessionStore.getOrganizationList() !== null){
+        console.log('updateOrganizations', hash(organizationList));
+        console.log('updateOrganizations', hash(response.body.userProfile.organizations));
+        if(hash(sessionStore.getOrganizationList()) !== hash(organizationList)){
+            sessionStore.setOrganizationList(organizationList);
+        }
+    }else{
+        sessionStore.setOrganizationList(organizationList);
+    }
+     
     return response;
 };
