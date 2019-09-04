d5f5c70d46e1f6f4d2ec8af7ddcf50e95c877e7e
aslan-mxc <aslan@mxc.org>
2019-09-04 20:18:11 +0200

    DB-agg_device_usage  implementing relation definition and basic queries

---
+++ m2m/db/postgres_db/pg_agg_device_usage.go
@@ -0,0 +1,62 @@
+package postgres_db
+
+import (
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+	types "gitlab.com/MXCFoundation/cloud/mxprotocol-server/m2m/types"
+)
+
+func (pgDbp *PGHandler) CreateAggDvUsgTable() error {
+	_, err := pgDbp.DB.Exec(`
+	
+		CREATE TABLE IF NOT EXISTS agg_device_usage (
+			id SERIAL PRIMARY KEY,
+			fk_device INT REFERENCES device (id) NOT NULL,
+			fk_agg_wallet_usage INT , -- REFERENCES agg_wallet_usage (id)   @@ to be added
+			dl_cnt INT    DEFAULT 0 ,
+			ul_cnt     INT DEFAULT 0,
+			dl_cnt_free    INT DEFAULT 0,
+			ul_cnt_free INT  DEFAULT 0,
+			dl_size_sum  FLOAT    DEFAULT 0,
+			ul_size_sum  FLOAT DEFAULT 0,
+			start_at TIMESTAMP NOT NULL,
+			duration_minutes   INT ,
+			cost  NUMERIC(28,18) DEFAULT 0
+		);		
+	`)
+	return errors.Wrap(err, "db/pg_agg_device_usage/CreateAggDvUsgTable")
+}
+
+func (pgDbp *PGHandler) InsertAggDvUsg(adu types.AggDvUsg) (insertIndex int64, err error) {
+	err = pgDbp.DB.QueryRow(`
+		INSERT INTO agg_device_usage (
+			fk_device ,
+			fk_agg_wallet_usage ,
+			dl_cnt ,
+			ul_cnt ,
+			dl_cnt_free ,
+			ul_cnt_free ,
+			dl_size_sum ,
+			ul_size_sum ,
+			start_at ,
+			duration_minutes ,
+			cost  
+			) 
+		VALUES 
+			($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)
+		RETURNING id ;
+	`,
+		adu.FkDevice,
+		adu.FkAggWalletUsg,
+		adu.DlCnt,
+		adu.UlCnt,
+		adu.DlCntFree,
+		adu.UlCntFree,
+		adu.DlSizeSum,
+		adu.UlSizeSum,
+		adu.StartAt,
+		adu.DurationMinutes,
+		adu.Cost,
+	).Scan(&insertIndex)
+	return insertIndex, errors.Wrap(err, "db/pg_agg_device_usage/InsertAggDvUsg")
+}
