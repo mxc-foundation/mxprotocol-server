1aadc0baa338d62ff0ef765c9594b54e5037e486
NamGyeong <namgyeong.cho@mxc.org>
2019-09-13 17:02:37 +0200

    api deleted

--- m2m/api/appserver/gen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-GRPC_GW_PATH=`go list -f '{{ .Dir }}' github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway`
-GRPC_GW_PATH="${GRPC_GW_PATH}/../third_party/googleapis"
-
-# generate the gRPC code
-protoc -I. -I${GRPC_GW_PATH} -I../../api/m2m --go_out=paths=source_relative,plugins=grpc:. \
-    inner_gateway.proto \
-    inner_device.proto
