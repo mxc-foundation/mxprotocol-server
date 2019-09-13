deebe86f92564d537b8242c35ef4ab7b4bf27abc
NamGyeong <namgyeong.cho@mxc.org>
2019-09-13 17:03:27 +0200

    copy api

---
+++ m2m/api/appserver/gen.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+GRPC_GW_PATH=`go list -f '{{ .Dir }}' github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway`
+GRPC_GW_PATH="${GRPC_GW_PATH}/../third_party/googleapis"
+
+# generate the gRPC code
+protoc -I. -I${GRPC_GW_PATH} -I../../api/m2m --go_out=paths=source_relative,plugins=grpc:. \
+    inner_gateway.proto \
+    inner_device.proto
