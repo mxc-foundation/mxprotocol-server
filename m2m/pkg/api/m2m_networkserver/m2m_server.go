31ce1443919a844d07073db291b53eeb7f229aae
CoderShiun <123657@gmail.com>
2019-10-08 17:13:26 +0200

    improve the logic

--- m2m/pkg/api/m2m_networkserver/m2m_server.go
+++ m2m/pkg/api/m2m_networkserver/m2m_server.go
@@ -2,7 +2,6 @@ package m2m_networkserver
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	log "github.com/sirupsen/logrus"
@@ -35,17 +34,17 @@ func (*M2MNetworkServerAPI) DvUsageMode(ctx context.Context, req *networkserver.
 		return &networkserver.DvUsageModeResponse{}, err
 	}
 
-	_, gwMac, err := db.Gateway.GetFreeGwList(dvWalletId)
-	if err != nil {
-		log.WithError(err).Error("db/cannot get gwListOfWallet")
-		return &networkserver.DvUsageModeResponse{}, err
-	}
-
 	switch string(dvMode) {
 	case networkserver.DeviceMode_name[int32(networkserver.DeviceMode_DV_INACTIVE)]:
 		return &networkserver.DvUsageModeResponse{DvMode: networkserver.DeviceMode_DV_INACTIVE}, nil
 
 	case networkserver.DeviceMode_name[int32(networkserver.DeviceMode_DV_FREE_GATEWAYS_LIMITED)]:
+		_, gwMac, err := db.Gateway.GetFreeGwList(dvWalletId)
+		if err != nil {
+			log.WithError(err).Error("db/cannot get gwListOfWallet")
+			return &networkserver.DvUsageModeResponse{}, err
+		}
+
 		for _, v := range gwMac {
 			fgws := networkserver.GwMac{}
 			fgws.GwMac = v
@@ -62,9 +61,16 @@ func (*M2MNetworkServerAPI) DvUsageMode(ctx context.Context, req *networkserver.
 			log.WithError(err).Error("db/cannot get walletId")
 			return &networkserver.DvUsageModeResponse{}, err
 		}
+
 		balance, err := db.Wallet.GetWalletBalance(walletId)
 		if err != nil {
 			log.WithError(err).Error("db/cannot get balance")
+			return &networkserver.DvUsageModeResponse{}, err
+		}
+
+		_, gwMac, err := db.Gateway.GetFreeGwList(dvWalletId)
+		if err != nil {
+			log.WithError(err).Error("db/cannot get gwListOfWallet")
 			return &networkserver.DvUsageModeResponse{}, err
 		}
 
@@ -74,8 +80,8 @@ func (*M2MNetworkServerAPI) DvUsageMode(ctx context.Context, req *networkserver.
 			resp.FreeGwMac = append(resp.FreeGwMac, &fgws)
 		}
 
-		if balance > dlPrice {
-			resp.DvMode = networkserver.DeviceMode_DV_WHOLE_NETWORK
+		resp.DvMode = networkserver.DeviceMode_DV_WHOLE_NETWORK
+		if balance < dlPrice {
 			resp.EnoughBalance = false
 		} else {
 			resp.EnoughBalance = true
@@ -104,73 +110,80 @@ func (*M2MNetworkServerAPI) GwUsageMode(ctx context.Context, req *networkserver.
 }
 
 func (*M2MNetworkServerAPI) DlPktSent(ctx context.Context, req *networkserver.DlPktSentRequest) (*networkserver.DlPktSentResponse, error) {
+	//create a new thread for db
+	go func()(error) {
+		log.WithFields(log.Fields{
+			"DlPktId": req.DlPkt.DlIdNs,
+		}).Debug("grpc_api/DlPktSent")
 
-	fmt.Println("-- dl packet sent req: ", req)
-	log.WithFields(log.Fields{
-		"DlPktId": req.DlPkt.DlIdNs,
-	}).Debug("grpc_api/DlPktSent")
+		var dlPkt = types.DlPkt{}
+		dlPkt.DlIdNs = req.DlPkt.DlIdNs
+		dlPkt.Category = types.DlCategory(req.DlPkt.Category)
+		dlPkt.Nonce = req.DlPkt.Nonce
 
-	var dlPkt = types.DlPkt{}
-	dlPkt.DlIdNs = req.DlPkt.DlIdNs
-	dlPkt.Category = types.DlCategory(req.DlPkt.Category)
-	dlPkt.Nonce = req.DlPkt.Nonce
-
-	if createAt, err := time.Parse(timeLayout, req.DlPkt.CreateAt); err != nil {
-		log.WithError(err).Error("time format error")
-		return &networkserver.DlPktSentResponse{}, err
-	} else {
-		dlPkt.CreatedAt = createAt
-	}
-
-	dlPkt.Size = req.DlPkt.Size
-	//dlPkt.Token = req.DlPkt.TokenDlFrm1
-
-	_, err := db.DlPacket.InsertDlPkt(dlPkt)
-	if err != nil {
-		log.WithError(err).Error("db/cannot update dlPkt")
-		return &networkserver.DlPktSentResponse{}, err
-	}
-
-	dvId, err := db.Device.GetDeviceIdByDevEui(req.DlPkt.DevEui)
-	if err != nil {
-		log.WithError(err).Error("db/cannot get devID")
-		return &networkserver.DlPktSentResponse{}, err
-	}
-
-	err = db.Device.UpdateDeviceLastSeen(dvId, time.Now())
-	if err != nil {
-		log.WithError(err).Error("db/cannot update devicelastseen")
-		return &networkserver.DlPktSentResponse{}, err
-	}
-
-	gwId, err := db.Gateway.GetGatewayIdByMac(req.DlPkt.GwMac)
-	if err != nil {
-		log.WithError(err).Error("db/cannot get gwID")
-		return &networkserver.DlPktSentResponse{}, err
-	}
-
-	dvwalletId, err := db.Device.GetWalletIdOfDevice(dvId)
-	if err != nil {
-		log.WithError(err).Error("db/cannot get dvWalletID")
-		return &networkserver.DlPktSentResponse{}, err
-	}
-
-	gwwalletId, err := db.Gateway.GetWalletIdOfGateway(gwId)
-	if err != nil {
-		log.WithError(err).Error("db/cannot get gwWalletID")
-		return &networkserver.DlPktSentResponse{}, err
-	}
-
-	if dvwalletId == gwwalletId {
-		return &networkserver.DlPktSentResponse{}, nil
-	} else {
-		err = db.Wallet.TmpBalanceUpdatePktTx(dvId, gwId, dlPrice)
-		if err != nil {
-			log.WithError(err).Error("db/cannot update balance")
-			return &networkserver.DlPktSentResponse{}, err
+		if createAt, err := time.Parse(timeLayout, req.DlPkt.CreateAt); err != nil {
+			log.WithError(err).Error("time format error")
+			return err
+		} else {
+			dlPkt.CreatedAt = createAt
 		}
-	}
 
+		dlPkt.Size = req.DlPkt.Size
+		dlPkt.TokenDlFrm1 = req.DlPkt.TokenDlFrm1
+		dlPkt.TokenDlFrm2 = req.DlPkt.TokenDlFrm2
+
+		dvId, err := db.Device.GetDeviceIdByDevEui(req.DlPkt.DevEui)
+		if err != nil {
+			log.WithError(err).Error("db/cannot get devID")
+			return err
+		}
+
+		dlPkt.FkDevice = dvId
+
+		gwId, err := db.Gateway.GetGatewayIdByMac(req.DlPkt.GwMac)
+		if err != nil {
+			log.WithError(err).Error("db/cannot get gwID")
+			return err
+		}
+
+		dlPkt.FkGateway = gwId
+
+		_, err = db.DlPacket.InsertDlPkt(dlPkt)
+		if err != nil {
+			log.WithError(err).Error("db/cannot update dlPkt")
+			return err
+		}
+
+		err = db.Device.UpdateDeviceLastSeen(dvId, time.Now())
+		if err != nil {
+			log.WithError(err).Error("db/cannot update devicelastseen")
+			return err
+		}
+
+		dvwalletId, err := db.Device.GetWalletIdOfDevice(dvId)
+		if err != nil {
+			log.WithError(err).Error("db/cannot get dvWalletID")
+			return err
+		}
+
+		gwwalletId, err := db.Gateway.GetWalletIdOfGateway(gwId)
+		if err != nil {
+			log.WithError(err).Error("db/cannot get gwWalletID")
+			return err
+		}
+
+		if dvwalletId == gwwalletId {
+			return nil
+		} else {
+			err = db.Wallet.TmpBalanceUpdatePktTx(dvId, gwId, dlPrice)
+			if err != nil {
+				log.WithError(err).Error("db/cannot update balance")
+				return err
+			}
+		}
+
+		return nil
+	}()
 	return &networkserver.DlPktSentResponse{}, nil
 }
 
