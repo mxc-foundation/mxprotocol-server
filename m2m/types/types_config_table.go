46c218dd12b42e6e5c667cce531b2b7d4a722301
aslan-mxc <aslan@mxc.org>
2019-10-08 22:21:01 +0200

    remove extra id property of configTable

--- m2m/types/types_config_table.go
+++ m2m/types/types_config_table.go
@@ -3,7 +3,6 @@ package types
 import "time"
 
 type ConfigTable struct {
-	Id         int64     `db:"id"`
 	Key        string    `db:"key"`
 	Value      string    `db:"value"`
 	UpdateTime time.Time `db:"update_time"`
