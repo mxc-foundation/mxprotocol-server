647cfa84a5d8134ac907dc2abcd19ad09f33a1fe
Shiun Chen <123657@gmail.com>
2019-11-14 16:09:31 +0100

    del testing

--- m2m/pkg/services/staking/staking_controller.go
+++ m2m/pkg/services/staking/staking_controller.go
@@ -1,7 +1,6 @@
 package staking
 
 import (
-	"fmt"
 	"github.com/robfig/cron"
 	log "github.com/sirupsen/logrus"
 	"gitlab.com/MXCFoundation/cloud/mxprotocol-server/m2m/db"
@@ -13,7 +12,7 @@ func Setup(conf config.MxpConfig) error {
 	log.Info("cron task begin...")
 	c := cron.New()
 	//first day of the month at 12:00 (24-hour)
-	err := c.AddFunc("0 35 13 14 * ?", func() { //c.AddFunc("0 0 12 1 * ?", func() {
+	err := c.AddFunc("0 0 12 1 * ?", func() {
 		log.Info("Start stakingRevenueExec")
 		go func() {
 			err := stakingRevenueExec(conf)
@@ -34,8 +33,6 @@ func Setup(conf config.MxpConfig) error {
 func stakingRevenueExec(conf config.MxpConfig) error {
 	//get income from DB, since is one month ago.
 	//superNodeIncome (since time.Time, until time.Time)
-	//Todo: only for the test
-	income := 100.00
 
 	t := time.Now()
 	//first date of month 00:00:00
@@ -43,10 +40,10 @@ func stakingRevenueExec(conf config.MxpConfig) error {
 	//last date of month 23:59:59
 	endTime := startTime.AddDate(0, 1, 0).Add(time.Second * -1)
 
-	/*income, err := db.Wallet.GetSupernodeIncomeAmount(t, endTime)
+	income, err := db.Wallet.GetSupernodeIncomeAmount(t, endTime)
 	if err != nil {
 		log.WithError(err).Error("stakingRevenueExec/Cannot get income from DB")
-	}*/
+	}
 
 	// how many days in this month
 	lastDate := startTime.AddDate(0, 1, 0)
@@ -92,34 +89,18 @@ func stakingRevenueExec(conf config.MxpConfig) error {
 
 		//how much revenue this person should get.
 		revenueAmount := (income * conf.Staking.StakingPercentage) * (j.Amount * stakingTimePortion) / totalPortion
-		//Todo: only for the test
-		fmt.Println("j.Amount: ", j.Amount)
-		fmt.Println("stakingTimePortion: ", stakingTimePortion)
-		fmt.Println("StakingPercentage: ", conf.Staking.StakingPercentage)
 
 		//update revenue to DB.
 		_, err := db.StakeRevenue.InsertStakeRevenue(j.Id, stakeRevPeriodId, revenueAmount)
 		if err != nil {
 			log.WithError(err).Error("stakingRevenueExec/Cannot update revenue to DB")
 		}
-
-		//Todo: only for the testing
-		fmt.Println("stakingDays: ", stakingHours/24)
-		fmt.Println("revenueAmount: ", revenueAmount)
 	}
 
 	//when all the process finished, give the time to DB.
 	if err := db.StakeRevenuePeriod.UpdateCompletedStakeRevenuePeriod(stakeRevPeriodId); err != nil {
 		log.WithError(err).Error("stakingRevenueExec/Cannot update revenueTime to DB")
 	}
-
-	//Todo: only for the testing
-	fmt.Println("income: ", income)
-	fmt.Println("startTime: ", startTime)
-	fmt.Println("endTime: ", endTime)
-	fmt.Println("lastDate: ", lastDate)
-	fmt.Println("stakingRevDays: ", stakingRevDays)
-	fmt.Println("TotalProtaion: ", totalPortion)
 
 	return nil
 }
