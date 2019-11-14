fdd4f665869a25e855b65761115ad841df7052b0
lixuan <lixuan@mxc.org>
2019-11-14 18:59:03 +0100

    modify style

--- m2m/ui/src/views/ControlPanel/history/NetworkActivities.js
+++ m2m/ui/src/views/ControlPanel/history/NetworkActivities.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import { withRouter } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
+import TableCell from "@material-ui/core/TableCell";
+import theme from "../../../theme";
 
 const styles = {
     maxW140: {
@@ -28,10 +30,25 @@ class SuperNodeNetworkActivityHistory extends Component {
     render() {
         return(
             <Grid container spacing={24}>
-                <Grid item xs={12}>
-                    <a href={"https://www.google.de"} target="_blank">
-                        Coming soon, click here for more information.
-                    </a>
+                <Grid item xs={6}>
+                    <TableCell align={this.props.align}>
+                    <span style={
+                        {
+                            textDecoration: "none",
+                            color: theme.palette.primary.main,
+                            cursor: "pointer",
+                            padding: 0,
+                            fontWeight: "bold",
+                            fontSize: 20,
+                            opacity: 0.7,
+                            "&:hover": {
+                                opacity: 1,
+                            }
+                        }
+                    } className={this.props.classes.link} onClick={()=>{
+                        window.open("www.mxc.org", '_blank');
+                    }}>Coming soon, more information about network activity history, please click here</span>
+                    </TableCell>
                 </Grid>
             </Grid>
         );
--- m2m/ui/src/views/ControlPanel/withdraw/withdraw.js
+++ m2m/ui/src/views/ControlPanel/withdraw/withdraw.js
@@ -6,6 +6,9 @@ import TitleBarTitle from "../../../components/TitleBarTitle";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "../../withdraw/WithdrawStyle"
 import Button from "@material-ui/core/Button";
+import theme from "../../../theme";
+import TableCell from "@material-ui/core/TableCell";
+
 
 class SuperAdminWithdraw extends Component {
     constructor(props) {
@@ -13,34 +16,34 @@ class SuperAdminWithdraw extends Component {
         this.state = {};
     }
 
-    loadData = async () => {
-
-    };
-
-    componentDidMount() {
-        this.loadData();
-    }
-
-    componentDidUpdate(oldProps) {
-        if (this.props === oldProps) {
-            return;
-        }
-        this.loadData();
-    }
-
     render() {
         return (
             <Grid container spacing={24} className={this.props.classes.backgroundColor}>
-            <Grid item xs={12} className={this.props.classes.divider}>
+                {/*<Grid item xs={12} className={this.props.classes.divider}>
                 <div className={this.props.classes.TitleBar}>
                     <TitleBarTitle title="Withdraw" />
                 </div>
-            </Grid>
+            </Grid>*/}
 
             <Grid item xs={6}>
-                <a href={"https://www.google.de"} target="_blank">
-                    Coming soon, click here for more information.
-                </a>
+                <TableCell align={this.props.align}>
+                    <span style={
+                        {
+                            textDecoration: "none",
+                            color: theme.palette.primary.main,
+                            cursor: "pointer",
+                            padding: 0,
+                            fontWeight: "bold",
+                            fontSize: 20,
+                            opacity: 0.7,
+                            "&:hover": {
+                                opacity: 1,
+                            }
+                        }
+                    } className={this.props.classes.link} onClick={()=>{
+                        window.open("www.mxc.org", '_blank');
+                    }}>Coming soon, more information about withdraw, please click here</span>
+                </TableCell>
             </Grid>
 
             </Grid>
