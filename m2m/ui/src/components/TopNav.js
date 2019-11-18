09a5f02011bb01be5f5283fa3bbba9144bbe4f64
lixuan <lixuan@mxc.org>
2019-11-18 19:39:43 +0100

    replace lora with lpwan

--- m2m/ui/src/components/TopNav.js
+++ m2m/ui/src/components/TopNav.js
@@ -157,7 +157,7 @@ class TopNav extends Component {
           </IconButton> */}
 
           <div className={this.props.classes.flex}>
-            <img src="/logo/m2m_logo.png" className={this.props.classes.logo} alt="LoRa Server" />
+            <img src="/logo/m2m_logo.png" className={this.props.classes.logo} alt="LPWAN Server" />
           </div>
 
           {/* <div className={this.props.classes.flex}>
