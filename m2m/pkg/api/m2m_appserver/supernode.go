d5a62d9de32309a36220eaebb6373b381e9b95ac
lixuan <lixuan@mxc.org>
2019-12-16 18:44:50 +0100

    return err

--- m2m/pkg/api/m2m_appserver/supernode.go
+++ m2m/pkg/api/m2m_appserver/supernode.go
@@ -17,7 +17,7 @@ func (s *M2MServerAPI) AddSuperNodeMoneyAccount(ctx context.Context, in *api.Add
 	err := ext_account.UpdateActiveExtAccount(0, in.AccountAddr, api.Money_name[int32(in.MoneyAbbr)])
 	if err != nil {
 		log.WithError(err).Error("grpc_api/AddSuperNodeMoneyAccount")
-		return &api.AddSuperNodeMoneyAccountResponse{Status: false}, nil
+		return &api.AddSuperNodeMoneyAccountResponse{Status: false}, err
 	}
 
 	return &api.AddSuperNodeMoneyAccountResponse{Status: true}, nil
@@ -32,7 +32,7 @@ func (s *M2MServerAPI) GetSuperNodeActiveMoneyAccount(ctx context.Context, req *
 	accountAddr, err := ext_account.GetActiveExtAccount(0, api.Money_name[int32(req.MoneyAbbr)])
 	if err != nil {
 		log.WithError(err).Error("grpc_api/GetSuperNodeActiveMoneyAccount")
-		return &api.GetSuperNodeActiveMoneyAccountResponse{SupernodeActiveAccount: ""}, nil
+		return &api.GetSuperNodeActiveMoneyAccountResponse{SupernodeActiveAccount: ""}, err
 	}
 
 	return &api.GetSuperNodeActiveMoneyAccountResponse{SupernodeActiveAccount: accountAddr}, nil
